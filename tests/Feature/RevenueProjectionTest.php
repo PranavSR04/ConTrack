360e65a7944737ad1f4efd190b77b29db5317683
Pranav S R <psrdev04@gmail.com>
2024-03-19 19:11:48 +0530

    Fix:Notification

--- tests/Feature/RevenueProjectionTest.php
+++ tests/Feature/RevenueProjectionTest.php
@@ -9,7 +9,7 @@ use Tests\TestCase;
 class RevenueProjectionTest extends TestCase
 {
 
-    public function test_monthly_revenge_projection(): void {
+    public function test_monthly_revenue_projection(): void {
         $this->withoutMiddleware();
         $response = $this->getJson('/api/revenue/list?type=monthly');
         $response->assertJson([
@@ -62,7 +62,7 @@ class RevenueProjectionTest extends TestCase
 
 
     }
-    public function test_quarterly_revenge_projection(): void {
+    public function test_quarterly_revenue_projection(): void {
         $this->withoutMiddleware();
         $response = $this->getJson('/api/revenue/list?type=quarterly');
         $response->assertJson([
@@ -102,7 +102,7 @@ class RevenueProjectionTest extends TestCase
 
 
     }
-    public function test_yearly_revenge_projection(): void {
+    public function test_yearly_revenue_projection(): void {
         $this->withoutMiddleware();
         $response = $this->getJson('/api/revenue/list?type=yearly');
         $response->assertJson([
@@ -119,13 +119,104 @@ class RevenueProjectionTest extends TestCase
             ],
             'Total Revenue' => 36050000
         ]);
-        
-
-
-
+    
         $response->assertStatus(200);
 
 
     }
+
+    public function test_monthly_revenue_projection_with_filter_du(): void {
+        $this->withoutMiddleware();
+        $response = $this->getJson('/api/revenue/list/?type=monthly&du[]=DU1&du[]=DU2');
+        $response->assertJson([
+            'message' => 'Monthly Revenue Projection ',
+            'data' => [
+                'May, 2022' => 600000,
+                'July, 2022' => 800000,
+                'August, 2022' => 800000,
+                'March, 2023' => 400000,
+                'June, 2023' => 2100000,
+                'September, 2023' => 312500,
+                'October, 2023' => 800000,
+                'November, 2023' => 1300000,
+                'March, 2024' => 1000000,
+                'April, 2024' => 800000,
+                'May, 2024' => 800000,
+                'September, 2024' => 312500,
+                'October, 2024' => 300000,
+                'November, 2024' => 400000,
+                'December, 2024' => 1425000,
+                'March, 2025' => 400000,
+                'May, 2025' => 400000,
+                'July, 2025' => 600000,
+                'September, 2025' => 1000000,
+                'October, 2025' => 600000,
+                'February, 2026' => 500000,
+                'March, 2026' => 800000,
+                'May, 2026' => 400000,
+                'July, 2026' => 1000000,
+                'December, 2026' => 800000,
+                'January, 2027' => 600000,
+                'March, 2027' => 500000,
+                'May, 2027' => 500000,
+                'November, 2027' => 500000
+            ],
+            'Total Revenue' => 20750000
+        ]);
+        $response->assertStatus(200);
+        
+    }
+
+    public function test_induvidual_monthly_revenue_projection(): void {
+        $this->withoutMiddleware();
+        $response = $this->getJson('/api/revenue/list/1?type=monthly');
+        $response->assertJson([
+            'message' => 'Monthly Revenue Projection ',
+            'data' => [
+                'October, 2024' => 300000,
+                'October, 2025' => 600000,
+                'January, 2027' => 600000
+            ],
+            'Total Revenue' => 1500000
+        ]);
+        
+        $response->assertStatus(200);
+
+    }
+
+    public function test_induvidual_yearly_revenge_projection(): void {
+        $this->withoutMiddleware();
+        $response = $this->getJson('/api/revenue/list/1?type=yearly');
+        $response->assertJson([
+            'message' => 'Yearly Revenue Projection ',
+            'data' => [
+                '2024' => 300000,
+                '2025' => 600000,
+                '2027' => 600000
+            ],
+            'Total Revenue' => 1500000
+        ]);
+        
+        $response->assertStatus(200);
+    }
+
+    public function test_induvidual_quarterly_revenge_projection(): void {
+        $this->withoutMiddleware();
+        $response = $this->getJson('/api/revenue/list/1?type=quarterly');
+        $response->assertJson([
+            'message' => 'Quarterly Revenue Projection ',
+            'data' => [
+                '2024-Q4' => 300000,
+                '2025-Q4' => 600000,
+                '2027-Q1' => 600000
+            ],
+            'Total Revenue' => 1500000
+        ]);
+        
+
+        $response->assertStatus(200);
+
+    }
+
     
 }
