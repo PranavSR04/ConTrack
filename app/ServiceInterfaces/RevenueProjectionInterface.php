aa77d4d1d7c633ec5ea202bf97917b7f134676cb
Pranav S R <psrdev04@gmail.com>
2024-03-20 18:40:54 +0530

    Fix:Dashboard

--- app/ServiceInterfaces/RevenueProjectionInterface.php
+++ app/ServiceInterfaces/RevenueProjectionInterface.php
@@ -5,10 +5,19 @@ use Illuminate\Http\Request;
 
 interface RevenueProjectionInterface
 {
+    // Function for calculating the revenue projection overall
     public function revenueProjection(Request $request, $contract_id = null);
+
+    // Function for calculating yearly projection
     public function calculateYearlyProjection($Milestones, $revenueProjections, $totalAmount);
+
+    // Function for calculating quarterly projection
     public function calculateQuarterlyProjection($Milestones, $revenueProjections, $totalAmount);
+
+    // Function for calculating monthly projection
     public function calculateMonthlyProjection($Milestones, $revenueProjections, $totalAmount);
+
+    // Function for getting the final response 
     public function getResponse($projectionType, $revenueProjections, $totalAmount);
 
 }
