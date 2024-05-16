4607e2280b1ea2ebf94700a8a18610310247eeff
Pranav S R <psrdev04@gmail.com>
2024-05-16 16:49:48 +0530

    Feat: Session Expired Page

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -102,7 +102,7 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => auth()->factory()->getTTL() * 60,
+            'expires_in' => auth()->factory()->getTTL() ,
             'user' => $user,
             'user_id' => $user->id,
             'contrackUser' =>User::where("experion_id", $user->id)->first()
